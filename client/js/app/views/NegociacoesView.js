08794794e15bb06e74a0832ef523c2a0a4512812
Nicolas Paiva <Nicolas Paiva>
2017-04-21 17:57:00 -0300

    Herança, criação de view, criação de helper de data

---
+++ client/js/app/views/NegociacoesView.js
@@ -0,0 +1,33 @@
+class NegociacoesView extends View {
+    constructor(elemento) {
+        super(elemento);
+    }
+    template(model) {
+            return `<table class="table table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th>DATA</th>
+                            <th>QUANTIDADE</th>
+                            <th>VALOR</th>
+                            <th>VOLUME</th>
+                        </tr>
+                    </thead>
+                    ${model.negociacoes.map(negociacao => `
+                            <tr>
+                                <td>${DateHelper.dateParaTexto(negociacao.data)}</td>
+                                <td>${negociacao.quantidade}</td>
+                                <td>${negociacao.valor}</td>
+                                <td>${negociacao.volume}</td>
+                            </tr>
+                            `).join('')}
+                    <tbody>
+                    </tbody>
+                    <tfoot>
+                        <td colspan="3"></td>
+                        <td>
+                            ${model.negociacoes.reduce((total, negociacao) => total + negociacao.volume, 0)}
+                        </td>
+                    </tfoot>
+                </table>`;
+    }
+}
